b7602aa08923ca0432abd372018ee9ae10d9623a
Martelo Castro <jonathan.martelo@cencosud.com.co>
2024-10-19 10:40:46 -0500

    Cargue data

---
+++ TallerVistas/TallerVistas/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for TallerVistas project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TallerVistas.settings')
+
+application = get_asgi_application()
---
+++ TallerVistas/vistas/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class libros (models.Model):
+    Nombre = models.CharField(max_length=60)
+    Autor = models.CharField(max_length=60)
+    Año_Publicacion = models.CharField(max_length=60)
+    Editorial = models.CharField(max_length=60)
